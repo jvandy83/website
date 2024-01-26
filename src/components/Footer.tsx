84f851204e2e757da28bdd89d61936a627b70e57
jvandy83 <vanthedev@gmail.com>
2024-01-26 15:40:36 -0700

    removed png from gitattributes file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import linkedinIcon from '../assets/images/linkedin-icon.png';
-import githubIcon from '../assets/images/github-icon.png';
-import twitterIcon from '../assets/images/twitter-icon.png';
+import linkedinIcon from "../assets/icons/linkedin-icon.png";
+import githubIcon from "../assets/icons/github-icon.png";
+import twitterIcon from "../assets/icons/twitter-icon.png";
 
 export const Footer = () => {
-	return (
-		<footer>
-			<ul className='bg-black flex justify-around w-screen items-center h-24'>
-				<li className='w-16'>
-					<a href='https://linkedin.com/in/vanthedev'>
-						<img src={linkedinIcon} alt='icon8.com' />
-					</a>
-				</li>
-				<li className='w-20'>
-					<a href='https://github.com/jvandy83'>
-						<img src={githubIcon} alt='icon8.com' />
-					</a>
-				</li>
-				<li className='w-12'>
-					<a href='https://twitter.com/vanthedev'>
-						<img src={twitterIcon} alt='icon8.com' />
-					</a>
-				</li>
-			</ul>
-		</footer>
-	);
+  return (
+    <footer>
+      <ul className="bg-black flex justify-around w-screen items-center h-24">
+        <li className="w-16">
+          <a href="https://linkedin.com/in/vanthedev">
+            <img src={linkedinIcon} alt="icon8.com" />
+          </a>
+        </li>
+        <li className="w-20">
+          <a href="https://github.com/jvandy83">
+            <img src={githubIcon} alt="icon8.com" />
+          </a>
+        </li>
+        <li className="w-12">
+          <a href="https://twitter.com/vanthedev">
+            <img src={twitterIcon} alt="icon8.com" />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
